7634be6eb75b911b076c0ab89d5575a8e2dcd848
EdgarAllenFro <vip3r295@gmail.com>
2018-04-28 18:54:46 -0500

    Initial build

---
+++ 5. Website - Drilldown/src/pph.js
@@ -0,0 +1,227 @@
+var json = [{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"12:00 AM","Predicted":649,"Actual":542},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"1:00 AM","Predicted":753,"Actual":753},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"2:00 AM","Predicted":854,"Actual":826},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"3:00 AM","Predicted":842,"Actual":779},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"4:00 AM","Predicted":788,"Actual":723},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"5:00 AM","Predicted":766,"Actual":789},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"6:00 AM","Predicted":740,"Actual":710},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"7:00 AM","Predicted":924,"Actual":824},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"8:00 AM","Predicted":1053,"Actual":1123},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"9:00 AM","Predicted":1102,"Actual":1000},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"10:00 AM","Predicted":1122,"Actual":1400},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"11:00 AM","Predicted":1223,"Actual":938},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"12:00 PM","Predicted":1332,"Actual":1200},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"1:00 PM","Predicted":1420,"Actual":1105},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"2:00 PM","Predicted":1557,"Actual":1234},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"3:00 PM","Predicted":1113,"Actual":1002},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"4:00 PM","Predicted":1337,"Actual":1337},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"5:00 PM","Predicted":876,"Actual":870},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"6:00 PM","Predicted":850,"Actual":735},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"7:00 PM","Predicted":730,"Actual":423},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"8:00 PM","Predicted":690,"Actual":643},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"9:00 PM","Predicted":687,"Actual":645},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"10:00 PM","Predicted":640,"Actual":502},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"ORHA","LocNum":16,"Date":"4/14/2018","Time":"11:00 PM","Predicted":612,"Actual":479},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"12:00 AM","Predicted":512,"Actual":568},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"1:00 AM","Predicted":634,"Actual":812},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"2:00 AM","Predicted":854,"Actual":786},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"3:00 AM","Predicted":842,"Actual":712},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"4:00 AM","Predicted":786,"Actual":813},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"5:00 AM","Predicted":426,"Actual":873},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"6:00 AM","Predicted":642,"Actual":712},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"7:00 AM","Predicted":799,"Actual":834},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"8:00 AM","Predicted":824,"Actual":913},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"9:00 AM","Predicted":856,"Actual":916},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"10:00 AM","Predicted":879,"Actual":937},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"11:00 AM","Predicted":903,"Actual":1035},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"12:00 PM","Predicted":934,"Actual":1002},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"1:00 PM","Predicted":1034,"Actual":1087},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"2:00 PM","Predicted":1067,"Actual":1079},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"3:00 PM","Predicted":1128,"Actual":1234},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"4:00 PM","Predicted":1003,"Actual":1054},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"5:00 PM","Predicted":973,"Actual":899},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"6:00 PM","Predicted":942,"Actual":643},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"7:00 PM","Predicted":932,"Actual":567},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"8:00 PM","Predicted":910,"Actual":912},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"9:00 PM","Predicted":834,"Actual":885},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"10:00 PM","Predicted":711,"Actual":678},
+{"Division":"US Ops","Region":"Eastern","District":"Big Apple","Location":"NYCA","LocNum":100,"Date":"4/14/2018","Time":"11:00 PM","Predicted":648,"Actual":716},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"12:00 AM","Predicted":435,"Actual":846},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"1:00 AM","Predicted":534,"Actual":513},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"2:00 AM","Predicted":652,"Actual":517},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"3:00 AM","Predicted":746,"Actual":513},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"4:00 AM","Predicted":913,"Actual":618},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"5:00 AM","Predicted":953,"Actual":1043},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"6:00 AM","Predicted":567,"Actual":637},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"7:00 AM","Predicted":689,"Actual":756},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"8:00 AM","Predicted":813,"Actual":886},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"9:00 AM","Predicted":913,"Actual":946},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"10:00 AM","Predicted":813,"Actual":678},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"11:00 AM","Predicted":888,"Actual":843},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"12:00 PM","Predicted":935,"Actual":913},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"1:00 PM","Predicted":1023,"Actual":846},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"2:00 PM","Predicted":1123,"Actual":844},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"3:00 PM","Predicted":1246,"Actual":834},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"4:00 PM","Predicted":1863,"Actual":779},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"5:00 PM","Predicted":1135,"Actual":913},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"6:00 PM","Predicted":1134,"Actual":943},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"7:00 PM","Predicted":1346,"Actual":1103},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"8:00 PM","Predicted":1246,"Actual":1034},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"9:00 PM","Predicted":1123,"Actual":1184},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"10:00 PM","Predicted":1002,"Actual":1239},
+{"Division":"US Ops","Region":"Eastern","District":"Capital","Location":"GAIA","LocNum":208,"Date":"4/14/2018","Time":"11:00 PM","Predicted":946,"Actual":1035},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"12:00 AM","Predicted":719,"Actual":643},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"1:00 AM","Predicted":843,"Actual":802},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"2:00 AM","Predicted":742,"Actual":613},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"3:00 AM","Predicted":802,"Actual":716},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"4:00 AM","Predicted":873,"Actual":743},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"5:00 AM","Predicted":889,"Actual":1023},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"6:00 AM","Predicted":954,"Actual":846},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"7:00 AM","Predicted":735,"Actual":513},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"8:00 AM","Predicted":713,"Actual":789},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"9:00 AM","Predicted":913,"Actual":813},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"10:00 AM","Predicted":1024,"Actual":943},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"11:00 AM","Predicted":1124,"Actual":863},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"12:00 PM","Predicted":1128,"Actual":1002},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"1:00 PM","Predicted":769,"Actual":643},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"2:00 PM","Predicted":1233,"Actual":1453},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"3:00 PM","Predicted":1342,"Actual":983},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"4:00 PM","Predicted":1002,"Actual":1433},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"5:00 PM","Predicted":876,"Actual":843},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"6:00 PM","Predicted":735,"Actual":613},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"7:00 PM","Predicted":786,"Actual":643},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"8:00 PM","Predicted":896,"Actual":843},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"9:00 PM","Predicted":534,"Actual":701},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"10:00 PM","Predicted":798,"Actual":846},
+{"Division":"US Ops","Region":"Eastern","District":"Bostonian","Location":"BEDA","LocNum":21,"Date":"4/14/2018","Time":"11:00 PM","Predicted":913,"Actual":763}];
+Chart.defaults.global.defaultFontFamily = "Verdana";
+
+
+var ActualData =  [];
+var ExpectedData =  [];
+var labels = [];
+
+var timeRange = 14;
+
+for (var i = 0; i < timeRange; i++){
+ActualData.push(json[i].Actual);
+}
+
+for (var i = 0; i < timeRange; i++){
+ExpectedData.push(json[i].Predicted);
+}
+
+for (var i = 0; i < timeRange; i++){
+labels.push(json[i].Time);
+}
+
+
+var data = {
+    labels: labels,
+   
+   datasets: [
+        {
+            label: "Actual",
+            backgroundColor: 'rgba(255, 0, 0, 0.35)',
+            borderWidth: 2,
+            data: ActualData,
+            xAxisID: "bar-x-axis1",
+        },        
+        {
+            label: "Expected",
+            backgroundColor: 'rgba(0, 0, 255, 0.35)',
+            borderWidth: 2,
+            data: ExpectedData,
+            xAxisID: "bar-x-axis1",		
+        }
+    ]
+};
+
+var options= {
+
+
+  scales: {
+  
+ xAxes: [
+ 
+ 
+				{	
+				
+				
+                  stacked: true,
+                  id: "bar-x-axis1",
+                  barThickness : 30,
+				  
+                 
+				 
+				 ticks: {
+                    autoSkip: false,
+                    maxRotation: 0,
+                    minRotation: 35,
+					fontSize: 14,
+					display:false,
+                   
+                    
+                  }
+				  
+				  
+                  
+                },
+				
+				
+                {
+           gridLines: {
+                display: false,
+				offsetGridLines: true,
+              }
+		
+                }],
+                
+                
+      yAxes: [{
+
+	  
+	  
+	    labels:{
+			fontSize: 32,
+		},
+		
+        ticks: {
+          beginAtZero: true,
+		  fontSize: 16,
+		  
+          
+        },
+		
+		 scaleLabel: {
+        display: true,
+        labelString: 'Packages Recieved',
+		fontSize: 26,
+		
+      },
+	  
+      }]
+
+  }
+  
+  
+  
+};
+      
+    Chart.plugins.register
+    (
+    {
+        afterDatasetsUpdate: function (chart) {
+            Chart.helpers.each(chart.getDatasetMeta(0).data, function (rectangle, index) {
+                rectangle._view.width = rectangle._model.width = 15;
+            });
+        },
+    })
+
+var ctx = document.getElementById("myChart");
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: options
+});
+ 
